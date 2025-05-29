ace9ac7e1537c3d30ef4a82b81edcb58c4ab4235
sagivmaoz@gmail.com <sagivmaoz@gmail.com>
2025-05-30 02:16:48 +0300

    did some transitions and states

--- Tar4/states.zig
+++ Tar4/states.zig
@@ -1,3 +1,9 @@
+const std = @import("std");
+const fs = std.fs;
+const Writer = std.io.Writer;
+const mem = std.mem;
+
+
 const keywords = [_][]const u8{
     "class",  "constructor", "function", "method", "field",
     "static", "var",         "int",      "char",   "boolean",
@@ -10,11 +16,6 @@ const symbols = [_][]const u8{
     "{", "}", "(", ")", "[", "]", ".", ",", ";", "+", "-", "*", "/", "&", "|", "<", ">", "=", "~",
 };
 
-
-const std = @import("std");
-const fs = std.fs;
-const Writer = std.io.Writer;
-const mem = std.mem;
 
 
 pub fn q0(allocator: std.mem.Allocator, file_content: []u8, current_character: *u8, buffer : *std.ArrayList(u8), writer: Writer) !void {
@@ -84,15 +85,124 @@ pub fn q0(allocator: std.mem.Allocator, file_content: []u8, current_character: *
     
 }
 
-pub fn q1(allocator: std.mem.Allocator, file_content: []u8, current_character: *u8, buffer: *std.ArrayList(u8), writer: Writer) !void {}
+pub fn q1(allocator: std.mem.Allocator, file_content: []u8, current_character: *u8, buffer: *std.ArrayList(u8), writer: Writer) !void {
 
-pub fn q2(allocator: std.mem.Allocator, file_content: []u8, current_character: *u8, buffer: *std.ArrayList(u8), writer: Writer) !void {}
+    const _current = file_content[*current_character];
 
-pub fn q3(allocator: std.mem.Allocator, file_content: []u8, current_character: *u8, buffer: *std.ArrayList(u8), writer: Writer) !void {}
+    if((_current >= 'a' and _current <= 'z') or (_current >= 'A' and _current <= 'Z') or (_current >= '0' and _current <= '9') or _current == '_'){
+        
+        increment_character(_current, current_character, buffer);
 
-pub fn q4(allocator: std.mem.Allocator, file_content: []u8, current_character: *u8, buffer: *std.ArrayList(u8), writer: Writer) !void {}
+        q1(allocator,
+         file_content,
+          current_character,
+           buffer,
+            writer);
 
-pub fn q5(allocator: std.mem.Allocator, file_content: []u8, current_character: *u8, buffer: *std.ArrayList(u8), writer: Writer) !void {}
+        return;
+    }
+
+    const word = (buffer.*).items;
+
+    for(keywords) |keyword|{
+        if(std.mem.eql(u8, word, keyword)){
+            buffer_to_writer(buffer, "keyword", writer);
+            return;
+        }
+    }
+
+    buffer_to_writer(buffer, "identifier", writer);
+    return;
+    
+}
+
+pub fn q2(allocator: std.mem.Allocator, file_content: []u8, current_character: *u8, buffer: *std.ArrayList(u8), writer: Writer) !void {
+    const _current = file_content[*current_character];
+
+    if((_current >= '0' and _current <= '9')){
+        
+        increment_character(_current, current_character, buffer);
+
+        q2(allocator,
+         file_content,
+          current_character,
+           buffer,
+            writer);
+
+        return;
+    }
+
+    buffer_to_writer(buffer, "integerConstant", writer);
+    return;
+
+}
+
+pub fn q3(allocator: std.mem.Allocator, file_content: []u8, current_character: *u8, buffer: *std.ArrayList(u8), writer: Writer) !void {
+    const _current = file_content[*current_character];
+
+    if(_current != '"'){
+        
+        increment_character(_current, current_character, buffer);
+
+        q3(allocator,
+         file_content,
+          current_character,
+           buffer,
+            writer);
+
+        return;
+    }
+
+    increment_character(_current, current_character, buffer);
+
+        q4(allocator,
+         file_content,
+          current_character,
+           buffer,
+            writer);
+
+        return;
+
+}
+
+pub fn q4(allocator: std.mem.Allocator, file_content: []u8, current_character: *u8, buffer: *std.ArrayList(u8), writer: Writer) !void {
+    buffer_to_writer(buffer, "StringConstant", writer);
+    return;
+}
+
+pub fn q5(allocator: std.mem.Allocator, file_content: []u8, current_character: *u8, buffer: *std.ArrayList(u8), writer: Writer) !void {
+    const _current = file_content[*current_character];
+
+    if(_current == '/'){
+        
+        increment_character(_current, current_character, buffer);
+
+        q6(allocator,
+         file_content,
+          current_character,
+           buffer,
+            writer);
+
+        return;
+    }
+
+    if(_current == '*'){
+        
+        increment_character(_current, current_character, buffer);
+
+        q7(allocator,
+         file_content,
+          current_character,
+           buffer,
+            writer);
+
+        return;
+    }
+
+    buffer_to_writer(buffer, "symbol", writer);
+    return;
+
+}
 
 pub fn q6(allocator: std.mem.Allocator, file_content: []u8, current_character: *u8, buffer: *std.ArrayList(u8), writer: Writer) !void {}
 
@@ -113,6 +223,6 @@ fn increment_character(_current : u8,current_character: *u8, buffer : *std.Array
         return;
 }
 
-fn buffer_to_writer(buffer: *std.ArrayList(u8), writer: Writer) !void {
-    try writer.print("{s}", .{ (buffer.*).items });
+fn buffer_to_writer(buffer: *std.ArrayList(u8), lexeme: []u8, writer: Writer) !void {
+    try writer.print("<{s}>{s}</{s}>", .{lexeme, (buffer.*).items, lexeme});
 }
